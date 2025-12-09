e1e1a0dc0f2fe548d27e863699aaa47a2ceaffc3
grazielle-bastos <grazielle1101@gmail.com>
2025-12-09 20:23:21 -0300

    feat: create repository

---
+++ src/main/java/br/com/meli/api_social_meli/ApiSocialMeliApplication.java
@@ -0,0 +1,13 @@
+package br.com.meli.api_social_meli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSocialMeliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiSocialMeliApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/meli/api_social_meli/ApiSocialMeliApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meli.api_social_meli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSocialMeliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
